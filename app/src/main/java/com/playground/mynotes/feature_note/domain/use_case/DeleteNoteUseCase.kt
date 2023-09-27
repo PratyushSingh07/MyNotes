c1de99efaa7d66ace80f840dc113aca79a8fee33
Pratyush Singh <aries.pratyush@gmail.com>
2023-09-27 18:51:04 +0530

    feat: delete note use case

---
+++ app/src/main/java/com/playground/mynotes/feature_note/domain/use_case/DeleteNoteUseCase.kt
@@ -0,0 +1,14 @@
+package com.playground.mynotes.feature_note.domain.use_case
+
+import com.playground.mynotes.feature_note.domain.model.Note
+import com.playground.mynotes.feature_note.domain.repository.NoteRepository
+
+class DeleteNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(note: Note) {
+        repository.deleteNote(note)
+    }
+
+}
