d66c2a1d3e92c4b3831d06ac2a2c74b4083803aa
Pratyush Singh <aries.pratyush@gmail.com>
2023-09-27 12:19:28 +0530

    setup project structure

---
+++ app/src/main/java/com/playground/mynotes/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.playground.mynotes.feature_note.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playground.mynotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
