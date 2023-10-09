0e113538323ff1610049b65039d90a1e59a0bb5a
Pratyush Singh <aries.pratyush@gmail.com>
2023-10-09 22:01:59 +0530

    feat: get note by id use case

---
+++ app/src/main/java/com/playground/mynotes/feature_note/domain/use_case/GetNoteByIdUseCase.kt
@@ -0,0 +1,14 @@
+package com.playground.mynotes.feature_note.domain.use_case
+
+import com.playground.mynotes.feature_note.domain.model.Note
+import com.playground.mynotes.feature_note.domain.repository.NoteRepository
+
+class GetNoteByIdUseCase(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+
+}
