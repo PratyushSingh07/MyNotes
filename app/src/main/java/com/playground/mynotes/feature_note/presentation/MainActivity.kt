daed9cc9244b3e2f6110aa5bfe410b6c7ba01ea0
Pratyush Singh <aries.pratyush@gmail.com>
2023-10-09 18:49:27 +0530

    fix: missing hilt annotation

--- app/src/main/java/com/playground/mynotes/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/playground/mynotes/feature_note/presentation/MainActivity.kt
@@ -3,14 +3,19 @@ package com.playground.mynotes.feature_note.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.navigation.NavController
 import com.playground.mynotes.R
+import com.playground.mynotes.feature_note.presentation.note.NotesScreen
 import com.playground.mynotes.feature_note.presentation.note.components.NoteItem
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContent {
-
+//            NotesScreen()
         }
     }
 }
